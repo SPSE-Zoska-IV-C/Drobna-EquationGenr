cb8e657a892c7e8dfe7f8fae1a36c0c717d114b7
Calrisia <KatarinaDrobna@s.zochova.sk>
2025-11-18 20:26:26 +0100

    topics to study

---
+++ app/math_engine/equations/logarithmic.py
@@ -0,0 +1,12 @@
+from equations.root import Method
+
+from math import sqrt, log10
+from operator import sub, add, mul, truediv, pow
+from fractions import Fraction
+import sympy as sp
+import random
+
+### logarytmicke vety
+### substitucia 
+### uprava na rovnaky zaklad
+### podmienky
--- app/math_engine/equations/root.py
+++ app/math_engine/equations/root.py
@@ -6,29 +6,24 @@ from abc import ABC, abstractmethod
 class Method(ABC):
     def __init__(self, level):
         self.level = level
+        self.equation = None
+        self.roots = []
         self.steps = []
+
         self.create() # self.equation, self.roots, self.steps
 
     @abstractmethod
     def create(self):
         '''manages creation of equation, its roots and solving steps based on level, saved in self'''
         
-
     @abstractmethod
     def create_simple(self):
         '''Creates simple equation and solving steps, saved in self'''
         
-    
     @abstractmethod
     def create_advanced(self):
         '''Creates advanced equation and solving steps, saved in self'''
         
-
-    # @abstractmethod
-    # def solving_steps(self):
-    #     '''list of steps to solve equation'''
-    #     return list()
-    
     def get_roots(self):
         return self.roots
     
